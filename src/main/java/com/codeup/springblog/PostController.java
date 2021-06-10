ec28c16d2eb14ec5b74f6fe6c13e6b694a56974a
Jake Chadwell <jake.chadwell01@gmail.com>
2021-06-10 16:30:40 -0500

    completed view exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,30 +1,38 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
-    @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String post() {
-        return "posts index page";
+    @GetMapping(path = "/posts")
+    public String post(Model model) {
+        ArrayList<Post> postList = new ArrayList<>();
+        Post post3 = new Post("Post3", "This is yet another new post.");
+        Post post2 = new Post("Post2", "This is another new post.");
+        postList.add(post2);
+        postList.add(post3);
+        model.addAttribute("postList", postList);
+        return "posts/index";
     }
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String postId(@PathVariable String id) {
+    public String postId(@PathVariable String id, Model model) {
+        Post post = new Post("Post1", "This is a new post.");
+        model.addAttribute("post", post);
+        model.addAttribute("id", id);
         return "views post from " + id;
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
     public String postsCreate() {
         return "view the form for creating a post";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
     public String newPost() {
         return "create a new post";
     }
