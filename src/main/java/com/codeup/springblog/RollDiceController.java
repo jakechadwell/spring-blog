e42d2b12ffaf24e70785de009ecf1cf3e20be121
Jake Chadwell <jake.chadwell01@gmail.com>
2021-06-10 15:35:39 -0500

    completed dice

---
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -0,0 +1,28 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice")
+    public String roll(){
+        return "rolldice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String result(@PathVariable int guess, Model model) {
+        int diceRoll = (int) ((Math.random()*((6-1)+1))+1);
+        if(guess==diceRoll){
+            model.addAttribute("correct", true);
+        }else{
+            model.addAttribute("incorrect", false);
+        }
+        model.addAttribute("userGuess", guess);
+        model.addAttribute("diceRoll", diceRoll);
+        return "Result";
+    }
+}
